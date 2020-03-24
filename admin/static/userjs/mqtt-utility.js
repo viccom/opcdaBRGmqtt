254d238133ad184d8922c86715f76ca8dd384c49
viccom <3520845@qq.com>
2020-03-24 17:50:33 +0800

    first

---
+++ admin/static/userjs/mqtt-utility.js
@@ -0,0 +1,467 @@
+/*******************************************************************************
+ * Copyright (c) 2015 IBM Corp.
+ *
+ * All rights reserved. This program and the accompanying materials
+ * are made available under the terms of the Eclipse Public License v1.0
+ * and Eclipse Distribution License v1.0 which accompany this distribution.
+ *
+ * The Eclipse Public License is available at
+ *    http://www.eclipse.org/legal/epl-v10.html
+ * and the Eclipse Distribution License is available at
+ *   http://www.eclipse.org/org/documents/edl-v10.php.
+ *
+ * Contributors:
+ *    James Sutton - Initial Contribution
+ *******************************************************************************/
+
+/*
+Eclipse Paho MQTT-JS Utility
+This utility can be used to test the Eclipse Paho MQTT Javascript client.
+*/
+
+// Create a client instance
+mqtt_client =null;
+mqttc_connected = false;
+var mqtt_host = window.location.hostname;
+var mqtt_port = 3884;
+
+
+/**
+ *	临时兼容IE，去掉可变参数
+ */
+// function logMessage(type, content) {
+//     if (type === "INFO") {
+//         console.info(content);
+//     } else if (type === "ERROR") {
+//         console.error(content);
+//     } else {
+//         console.log(content);
+//     }
+// }
+
+/**
+ *	日志处理
+ */
+function logMessage(type, ...content) {
+    if (type === "INFO") {
+        console.info(...content);
+    } else if (type === "ERROR") {
+        console.error(...content);
+    } else {
+        console.log(...content);
+    }
+}
+
+
+
+function makeid() {
+    var text = "";
+    var possible = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789";
+
+    for (var i = 0; i < 8; i++)
+        text += possible.charAt(Math.floor(Math.random() * possible.length));
+
+    return text;
+}
+
+logMessage("INFO", "Starting Eclipse Paho JavaScript Utility.");
+
+// Things to do as soon as the page loads
+// document.getElementById("clientIdInput").value = "js-utility-" + makeid();
+
+var base64EncodeChars = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789+/";
+var base64DecodeChars = new Array(
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1,
+    -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, -1, 62, -1, -1, -1, 63,
+    52, 53, 54, 55, 56, 57, 58, 59, 60, 61, -1, -1, -1, -1, -1, -1,
+    -1,  0,  1,  2,  3,  4,  5,  6,  7,  8,  9, 10, 11, 12, 13, 14,
+    15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, -1, -1, -1, -1, -1,
+    -1, 26, 27, 28, 29, 30, 31, 32, 33, 34, 35, 36, 37, 38, 39, 40,
+    41, 42, 43, 44, 45, 46, 47, 48, 49, 50, 51, -1, -1, -1, -1, -1);
+
+function base64encode(str) {
+    var out, i, len;
+    var c1, c2, c3;
+
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+        c1 = str.charCodeAt(i++) & 0xff;
+        if(i == len)
+        {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt((c1 & 0x3) << 4);
+            out += "==";
+            break;
+        }
+        c2 = str.charCodeAt(i++);
+        if(i == len)
+        {
+            out += base64EncodeChars.charAt(c1 >> 2);
+            out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+            out += base64EncodeChars.charAt((c2 & 0xF) << 2);
+            out += "=";
+            break;
+        }
+        c3 = str.charCodeAt(i++);
+        out += base64EncodeChars.charAt(c1 >> 2);
+        out += base64EncodeChars.charAt(((c1 & 0x3)<< 4) | ((c2 & 0xF0) >> 4));
+        out += base64EncodeChars.charAt(((c2 & 0xF) << 2) | ((c3 & 0xC0) >>6));
+        out += base64EncodeChars.charAt(c3 & 0x3F);
+    }
+    return out;
+}
+
+function base64decode(str) {
+    var c1, c2, c3, c4;
+    var i, len, out;
+
+    len = str.length;
+    i = 0;
+    out = "";
+    while(i < len) {
+        /* c1 */
+        do {
+            c1 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        } while(i < len && c1 == -1);
+        if(c1 == -1)
+            break;
+
+        /* c2 */
+        do {
+            c2 = base64DecodeChars[str.charCodeAt(i++) & 0xff];
+        } while(i < len && c2 == -1);
+        if(c2 == -1)
+            break;
+
+        out += String.fromCharCode((c1 << 2) | ((c2 & 0x30) >> 4));
+
+        /* c3 */
+        do {
+            c3 = str.charCodeAt(i++) & 0xff;
+            if(c3 == 61)
+                return out;
+            c3 = base64DecodeChars[c3];
+        } while(i < len && c3 == -1);
+        if(c3 == -1)
+            break;
+
+        out += String.fromCharCode(((c2 & 0XF) << 4) | ((c3 & 0x3C) >> 2));
+
+        /* c4 */
+        do {
+            c4 = str.charCodeAt(i++) & 0xff;
+            if(c4 == 61)
+                return out;
+            c4 = base64DecodeChars[c4];
+        } while(i < len && c4 == -1);
+        if(c4 == -1)
+            break;
+        out += String.fromCharCode(((c3 & 0x03) << 6) | c4);
+    }
+    return out;
+}
+
+function utf16to8(str) {
+    var out, i, len, c;
+
+    out = "";
+    len = str.length;
+    for(i = 0; i < len; i++) {
+        c = str.charCodeAt(i);
+        if ((c >= 0x0001) && (c <= 0x007F)) {
+            out += str.charAt(i);
+        } else if (c > 0x07FF) {
+            out += String.fromCharCode(0xE0 | ((c >> 12) & 0x0F));
+            out += String.fromCharCode(0x80 | ((c >>  6) & 0x3F));
+            out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+        } else {
+            out += String.fromCharCode(0xC0 | ((c >>  6) & 0x1F));
+            out += String.fromCharCode(0x80 | ((c >>  0) & 0x3F));
+        }
+    }
+    return out;
+}
+
+function utf8to16(str) {
+    var out, i, len, c;
+    var char2, char3;
+
+    out = "";
+    len = str.length;
+    i = 0;
+    while(i < len) {
+        c = str.charCodeAt(i++);
+        switch(c >> 4)
+        {
+            case 0: case 1: case 2: case 3: case 4: case 5: case 6: case 7:
+            // 0xxxxxxx
+            out += str.charAt(i-1);
+            break;
+            case 12: case 13:
+            // 110x xxxx   10xx xxxx
+            char2 = str.charCodeAt(i++);
+            out += String.fromCharCode(((c & 0x1F) << 6) | (char2 & 0x3F));
+            break;
+            case 14:
+                // 1110 xxxx  10xx xxxx  10xx xxxx
+                char2 = str.charCodeAt(i++);
+                char3 = str.charCodeAt(i++);
+                out += String.fromCharCode(((c & 0x0F) << 12) |
+                    ((char2 & 0x3F) << 6) |
+                    ((char3 & 0x3F) << 0));
+                break;
+        }
+    }
+
+    return out;
+}
+
+function CharToHex(str) {
+    var out, i, len, c, h;
+
+    out = "";
+    len = str.length;
+    i = 0;
+    while(i < len)
+    {
+        c = str.charCodeAt(i++);
+        h = c.toString(16);
+        if(h.length < 2)
+            h = "0" + h;
+        out += h + " ";
+        /*                out += "\\x" + h + " ";
+                        if(i > 0 && i % 8 == 0)
+                            out += "\r\n";*/
+    }
+
+    return out;
+}
+
+
+// called when the client connects
+function onConnect(context) {
+  // Once a connection has been made, make a subscription and send a message.
+  var connectionString = context.invocationContext.host + ":" + context.invocationContext.port + context.invocationContext.path;
+  logMessage("INFO", "Connection Success ", "[URI: ", connectionString, ", ID: ", context.invocationContext.clientId, "]");
+  // var statusSpan = document.getElementById("connectionStatus");
+  // statusSpan.innerHTML = "Connected to: " + connectionString + " as " + context.invocationContext.clientId;
+  mqttc_connected = true;
+
+}
+
+
+function onConnected(reconnect=true, uri) {
+  // Once a connection has been made, make a subscription and send a message.
+  logMessage("INFO", "Client Has now connected: [Reconnected: ", reconnect, ", URI: ", uri, "]");
+  mqttc_connected = true;
+  mqtt_client.subscribe(['v1/opcdabrg/api/RESULT', 'v1/opcdabrg/OPCDABRG_DATAS/#', 'v1/opcdabrg/OPCDABRG_LOGS/#']);
+
+
+}
+
+function onFail(context) {
+  logMessage("ERROR", "Failed to connect. [Error Message: ", context.errorMessage, "]");
+  // var statusSpan = document.getElementById("connectionStatus");
+  // statusSpan.innerHTML = "Failed to connect: " + context.errorMessage;
+  mqttc_connected = false;
+}
+
+// called when the client loses its connection
+function onConnectionLost(responseObject) {
+  if (responseObject.errorCode !== 0) {
+    logMessage("INFO", "Connection Lost. [Error Message: ", responseObject.errorMessage, "]");
+  }
+  mqttc_connected = false;
+}
+
+// called when a message arrives
+function onMessageArrived(message) {
+    // console.log("topic: ",message.destinationName);
+    var arr_topic = message.destinationName.split("/");
+    // console.log(arr_topic[3]);
+    // logMessage("INFO", "Message Recieved: [Topic: ", message.destinationName, ", Payload: ", message.payloadString, ", QoS: ", message.qos, ", Retained: ", message.retained, ", Duplicate: ", message.duplicate, "]");
+    if(arr_topic[2]==="OPCDABRG_LOGS"){
+        console.log(message.payloadString);
+        var log_message = JSON.parse(message.payloadString);
+        // console.log(typeof log_message);
+        var local_datetime = new Date(parseInt(log_message[0]) * 1000).toLocaleString('chinese', { hour12: false });
+        // console.log(bj_datetime)
+        var new_log_message = [local_datetime, log_message[1], log_message[2]];
+        log_table.row.add(new_log_message).draw();
+
+    }else if(arr_topic[2]==="OPCDABRG_DATAS") {
+        // console.log(message.payloadString);
+        var data_message = JSON.parse(message.payloadString);
+        data_table.clear().draw();
+        for (i = 0; i < data_message.length; i++) {
+            data_table.row.add(data_message[i]).draw();
+        }
+        // console.log(typeof log_message[1]);
+    }
+    else if(arr_topic[3]==="RESULT"){
+        // console.log(message.payloadString);
+        var apiResult_message = JSON.parse(message.payloadString);
+        // console.log(apiResult_message['id']);
+
+        if(apiResult_message['id'].indexOf("getConfig") != -1 ){
+            $(".OPCServerName").text(apiResult_message['data']['opcname']);
+            $(".OPCServerHost").text(apiResult_message['data']['opchost']);
+        }
+        if(apiResult_message['id'].indexOf("tunnelStatus") != -1 ){
+            $(".OPCServerStatus").text(apiResult_message['data']);
+        }
+        // $(".MQTTStatus").text(apiResult_message['data']['clientid']);
+
+    }
+
+}
+
+function connectionToggle() {
+
+  if (connected) {
+    disconnect();
+  } else {
+    connect();
+  }
+
+}
+
+
+function connect() {
+  var hostname = mqtt_host;
+  var port = mqtt_port;
+  var clientId = "webclient-"+makeid();
+
+  var path = "/mqtt";
+  var user = '';
+  var pass = '';
+  // var pass = "Pa88word";
+  var keepAlive = 60;
+  var timeout = 3;
+  var tls = false;
+  var automaticReconnect = true;
+  var cleanSession = true;
+    var lastWillTopic = null;
+    var lastWillQos = 0;
+    var lastWillRetain = false;
+    var lastWillMessage = null;
+
+
+  if (path.length > 0) {
+    mqtt_client =new Paho.Client(hostname, Number(port), path, clientId);
+  } else {
+    mqtt_client =new Paho.Client(hostname, Number(port), clientId);
+  }
+  logMessage("INFO", "Connecting to Server: [Host: ", hostname, ", Port: ", port, ", Path: ", mqtt_client.path, ", ID: ", clientId, ", USER: ", user,", PASS: ", pass,"]");
+
+  // set callback handlers
+  mqtt_client.onConnectionLost = onConnectionLost;
+  mqtt_client.onMessageArrived = onMessageArrived;
+  mqtt_client.onConnected = onConnected;
+
+
+  var options = {
+    invocationContext: { host: hostname, port: port, path: mqtt_client.path, clientId: clientId },
+    timeout: timeout,
+    keepAliveInterval: keepAlive,
+    cleanSession: cleanSession,
+    useSSL: tls,
+    reconnect: true,
+    onSuccess: onConnect,
+    onFailure: onFail
+  };
+
+
+
+  if (user.length > 0) {
+    options.userName = user;
+  }
+
+  if (pass.length > 0) {
+    options.password = pass;
+  }
+
+  if (lastWillTopic) {
+    var lastWillMessage = new Paho.Message(lastWillMessage);
+    lastWillMessage.destinationName = lastWillTopic;
+    lastWillMessage.qos = lastWillQos;
+    lastWillMessage.retained = lastWillRetain;
+    options.willMessage = lastWillMessage;
+  }
+
+  // connect the client
+    try {
+        mqtt_client.connect(options);
+    } catch (error) {
+        mqttc_connected = false;
+        console.log(error);
+    }
+
+  console.log();
+
+
+  // var statusSpan = document.getElementById("connectionStatus");
+  // statusSpan.innerHTML = "Connecting...";
+}
+
+function disconnect() {
+  logMessage("INFO", "Disconnecting from Server.");
+  mqtt_client.disconnect();
+  // var statusSpan = document.getElementById("connectionStatus");
+  // statusSpan.innerHTML = "Connection - Disconnected.";
+  mqttc_connected = false;
+}
+
+
+function publish() {
+  var topic = document.getElementById("publishTopicInput").value;
+  var qos = document.getElementById("publishQosInput").value;
+  var message = document.getElementById("publishMessageInput").value;
+  var retain = document.getElementById("publishRetainInput").checked;
+  logMessage("INFO", "Publishing Message: [Topic: ", topic, ", Payload: ", message, ", QoS: ", qos, ", Retain: ", retain, "]");
+  message = new Paho.Message(message);
+  message.destinationName = topic;
+  message.qos = Number(qos);
+  message.retained = retain;
+  mqtt_client.send(message);
+}
+
+
+function subscribe() {
+  var topic = document.getElementById("subscribeTopicInput").value;
+  var qos = document.getElementById("subscribeQosInput").value;
+  logMessage("INFO", "Subscribing to: [Topic: ", topic, ", QoS: ", qos, "]");
+  mqtt_client.subscribe(topic, { qos: Number(qos) });
+}
+
+function unsubscribe() {
+  var topic = document.getElementById("subscribeTopicInput").value;
+  logMessage("INFO", "Unsubscribing: [Topic: ", topic, "]");
+  mqtt_client.unsubscribe(topic, {
+    onSuccess: unsubscribeSuccess,
+    onFailure: unsubscribeFailure,
+    invocationContext: { topic: topic }
+  });
+}
+
+
+function unsubscribeSuccess(context) {
+  logMessage("INFO", "Unsubscribed. [Topic: ", context.invocationContext.topic, "]");
+}
+
+function unsubscribeFailure(context) {
+  logMessage("ERROR", "Failed to unsubscribe. [Topic: ", context.invocationContext.topic, ", Error: ", context.errorMessage, "]");
+}
+
+function clearHistory() {
+  var table = document.getElementById("incomingMessageTable");
+  //or use :  var table = document.all.tableid;
+  for (var i = table.rows.length - 1; i > 0; i--) {
+    table.deleteRow(i);
+  }
+
+}
+
---
+++ admin/static/userjs/opcview.js
@@ -0,0 +1,371 @@
+var log_subscribed = false;
+
+
+setTimeout(function () {
+    connect();
+},600);
+
+/**
+ *	周期检测mqtt状态
+ */
+var mqtt_status_ret= setInterval(function(){
+    if(mqttc_connected){
+        $(".MQTTStatus").text("已连接");
+    }else{
+        $(".MQTTStatus").text("重连中");
+        connect();
+    }
+
+},2000);
+
+/**
+ *	周期获取opcdaBRG状态
+ */
+var opcdaBRG_status_ret= setInterval(function(){
+    if(mqttc_connected){
+        console.log('查询opcdaBRG状态');
+        message = new Paho.Message(JSON.stringify({"id":'getConfig/' + Date.parse(new Date()).toString()}));
+        message.destinationName = 'v1/opcdabrg/api/getConfig';
+        message.qos = 0;
+        mqtt_client.send(message);
+        message = new Paho.Message(JSON.stringify({"id":'tunnelStatus/' + Date.parse(new Date()).toString()}));
+        message.destinationName = 'v1/opcdabrg/api/tunnelStatus';
+        message.qos = 0;
+        mqtt_client.send(message);
+    }
+
+},2000);
+
+
+$(function () {
+
+
+    /**
+     *	初始化数据表格
+     */
+    data_table = $('#data_table').DataTable({
+        // "dom": '',
+        "filter": true,
+        "info": true,
+        "scrollY":        "400px",
+        "scrollCollapse": true,
+        "paging":         true,
+        "processing": true,
+        "bStateSave": false,
+        "order": [[ 0, "asc" ]],
+        "language": {
+            "sProcessing": "处理中...",
+            "sLengthMenu": "显示 _MENU_ 项结果",
+            "sZeroRecords": "没有匹配结果",
+            "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+            "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+            "sInfoPostFix": "",
+            "sSearch": "搜索:",
+            "sUrl": "",
+            "sEmptyTable": "消息为空",
+            "sLoadingRecords": "载入中...",
+            "sInfoThousands": ",",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "上页",
+                "sNext": "下页",
+                "sLast": "末页"
+            },
+            "oAria": {
+                "sSortAscending": ": 以升序排列此列",
+                "sSortDescending": ": 以降序排列此列"
+            }
+        },
+        columnDefs: [
+            {
+                //   指定第第1列
+                targets:  0,
+                "width": '20%',
+                searchable: true,
+                orderable: false
+
+            },
+            {
+                //   指定第第2列
+                targets:  1,
+                "width": '20%',
+                searchable: false,
+                orderable: false
+            },
+            {
+                //   指定第第4列
+                targets:  2,
+                "width": '10%',
+                searchable: true,
+                orderable: false
+            },
+            {
+                //   指定第第4列
+                targets:  3,
+                "width": '50%',
+                searchable: false,
+                orderable: false
+            }
+        ],
+        "initComplete": function(settings, json) {
+            console.log("data_table init over")
+        }
+    });
+
+
+    /**
+     *	初始化日志表格
+     */
+    log_table = $('#log_table').DataTable({
+        // "dom": '',
+        "filter": true,
+        "info": false,
+        // "scrollY":        "50px",
+        // "scrollCollapse": true,
+        "paging":         false,
+        "processing": true,
+        "bStateSave": false,
+        "order": [[ 0, "asc" ]],
+        "language": {
+            "sProcessing": "处理中...",
+            "sLengthMenu": "显示 _MENU_ 项结果",
+            "sZeroRecords": "没有匹配结果",
+            "sInfo": "显示第 _START_ 至 _END_ 项结果，共 _TOTAL_ 项",
+            "sInfoEmpty": "显示第 0 至 0 项结果，共 0 项",
+            "sInfoFiltered": "(由 _MAX_ 项结果过滤)",
+            "sInfoPostFix": "",
+            "sSearch": "搜索:",
+            "sUrl": "",
+            "sEmptyTable": "消息为空",
+            "sLoadingRecords": "载入中...",
+            "sInfoThousands": ",",
+            "oPaginate": {
+                "sFirst": "首页",
+                "sPrevious": "上页",
+                "sNext": "下页",
+                "sLast": "末页"
+            },
+            "oAria": {
+                "sSortAscending": ": 以升序排列此列",
+                "sSortDescending": ": 以降序排列此列"
+            }
+        },
+        columnDefs: [
+            {
+                //   指定第第1列
+                targets:  0,
+                "width": '15%',
+                searchable: false,
+                orderable: false
+
+            },
+            {
+                //   指定第第2列
+                targets:  1,
+                "width": '10%',
+                searchable: true,
+                orderable: false
+            },
+            {
+                //   指定第第4列
+                targets:  2,
+                "width": '75%',
+                searchable: true,
+                orderable: false
+            }
+        ],
+        "initComplete": function(settings, json) {
+            console.log("log_table init over")
+        }
+    });
+
+})
+
+/**
+ *	周期检测mqtt状态
+ */
+/*
+
+var mqtt_status_ret= setInterval(function(){
+    $(".log_nums").text(table_log.data().length);
+    if(mqttc_connected){
+        $(".appinfo-upload").text("停止");
+        $(".appinfo-upload").addClass("btn-warning");
+        $(".appinfo-upload").removeClass("btn-primary");
+    }else{
+        $(".appinfo-upload").text("连接");
+        $(".appinfo-upload").addClass("btn-primary");
+        $(".appinfo-upload").removeClass("btn-warning");
+    }
+    if(log_subscribed){
+        $(".btn-log-subscribe").text("取消订阅");
+        $(".btn-log-subscribe").addClass("btn-success");
+    }else{
+        $(".btn-log-subscribe").text("日志订阅");
+        $(".btn-log-subscribe").removeClass("btn-success");
+    }
+
+},1000);
+*/
+
+/**
+ *	周期上传日志报文
+ */
+
+/*
+
+var mqtt_upload_ret= setInterval(function(){
+    var lens=table_log.data().length;
+    if(lens<1000){
+        if(mqttc_connected){
+            if(log_subscribed){
+                var id = "sys_enable_log" + '/' + gate_sn + '/'+ Date.parse(new Date())
+                var _act = {
+                    "device": gate_sn,
+                    "data": 60,
+                    "id": id
+                };
+                gate_upload_mes("sys_enable_log", _act)
+            }
+
+        }
+    }else{
+        mqtt_client.unsubscribe(gate_sn + "/" + "log", {
+            onSuccess: unsubscribeSuccess,
+            onFailure: unsubscribeFailure,
+            invocationContext: { topic: gate_sn + "/" + "log" }
+        });
+
+        log_subscribed = false;
+
+        $.notify({
+            title: "<strong>记录数超过接收队列:</strong><br><br> ",
+            message: "日志和报文记录数已经超过1000，需清除后才可继续接收！"
+        },{
+            type: 'warning',
+            delay: 29000
+        });
+    }
+
+},30000);
+
+*/
+
+/**
+ *	开启日志和报文上传功能
+ */
+/*
+$(".btn-log-subscribe").click(function(){
+    var lens=table_log.data().length;
+    if(mqttc_connected){
+        if(log_subscribed){
+            mqtt_client.unsubscribe(gate_sn + "/" + "log", {
+                onSuccess: unsubscribeSuccess,
+                onFailure: unsubscribeFailure,
+                invocationContext: { topic: gate_sn + "/" + "log" }
+            });
+            log_subscribed = false;
+            $(".btn-log-subscribe").removeClass("btn-success");
+        }else{
+            try {
+                if(lens<1000){
+                    mqtt_client.subscribe(gate_sn + "/" + "log", {qos: 0});
+                    log_subscribed = true;
+                    $(".btn-log-subscribe").addClass("btn-success");
+                    var id = "sys_enable_log" + '/' + gate_sn + '/'+ Date.parse(new Date())
+                    var _act = {
+                        "device": gate_sn,
+                        "data": 60,
+                        "id": id
+                    };
+                    gate_upload_mes("sys_enable_log", _act)
+                }else{
+                    $.notify({
+                        title: "<strong>记录数超过接收队列:</strong><br><br> ",
+                        message: "日志和报文记录数已经超过1000，需清除后才可继续接收！"
+                    },{
+                        delay: 5000
+                    });
+                }
+
+            } catch (error) {
+                console.log(error);
+                mqttc_connected = false;
+                log_subscribed = false;
+                $(".btn-log-subscribe").removeClass("btn-success");
+            }
+        }
+
+    }
+});
+
+$(".appinfo-clear").click(function(){
+    table_log.clear().draw();
+});
+*/
+
+
+
+
+/**
+ *	过滤选择处理-start
+ */
+/*
+
+$('div.log_filter li').on('click', function(){
+    $('.search_log').text($(this).text());
+    $('input.J_keyword').val("");
+    table_log.columns( 1 ).search("").draw();
+    table_log.columns( 2 ).search("").draw();
+    table_log.columns( 3 ).search("").draw();
+    table_log.search("").draw();
+
+});
+*/
+
+
+
+/*
+
+$("input.J_keyword").bind("input propertychange",function(event){
+    var key = $('input.J_keyword').val();
+    var colnum = 3;
+
+    if($('.search_log').text()=="内容"){
+        colnum = 3
+    }
+    if($('.search_log').text()=="ID"){
+        colnum = 2
+    }
+    if($('.search_log').text()=="类型"){
+        colnum = 1
+    }
+
+    console.log(colnum, key);
+    if(key!=null){
+        table_log.columns( colnum ).search(key).draw();
+    }
+});
+
+$('button.J_keyword').on('keyup click', function () {
+    var key = $('input.J_keyword').val();
+    var colnum = 3;
+
+    if ($('.search_log').text() == "内容") {
+        colnum = 3
+    }
+    if ($('.search_log').text() == "ID") {
+        colnum = 2
+    }
+    if ($('.search_log').text() == "类型") {
+        colnum = 1
+    }
+
+
+    table_log.columns(colnum).search(key, false, true).draw();
+});
+*/
+/**
+ *	过滤选择处理-end
+ */
