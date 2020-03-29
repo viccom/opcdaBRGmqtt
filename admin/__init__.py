76bdfcba352714b72f2dff30d2874701094e82fd
viccom <3520845@qq.com>
2020-03-29 21:48:03 +0800

    add opcdaBRG logfile download

--- admin/__init__.py
+++ admin/__init__.py
@@ -1,5 +1,6 @@
-from flask import Flask, current_app,  g, session, redirect, url_for, escape, request, render_template, make_response
+from flask import Flask, current_app,  send_from_directory, redirect, url_for, escape, request, render_template, make_response
 from flask_cors import *
+from os import getcwd
 
 
 def start_admin(blueprints=[], services={}):
@@ -15,6 +16,14 @@ def start_admin(blueprints=[], services={}):
     @app.route("/")
     def index():
         return render_template("index.html")
+
+    @app.route('/getlogfile')
+    def getlogfile():
+        directory = getcwd()  # 假设在当前目录
+        filename = 'log.log'
+        response = make_response(send_from_directory(directory + '/log/', filename, as_attachment=True))
+        response.headers["Content-Disposition"] = "attachment; filename={}".format(filename.encode().decode('latin-1'))
+        return response
 
     CORS(app, supports_credentials=True)
     app.run(host="0.0.0.0", port=3080, debug=False)
